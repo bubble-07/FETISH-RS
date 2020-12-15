981814d8c4b2dfac581d2daebb146abc218a6c91
bubble-07 <spagrab@gmail.com>
2020-12-14 19:54:13 -0500

    Add fused kronecker->transform op for func scatter tensors

--- src/func_scatter_tensor.rs
+++ src/func_scatter_tensor.rs
@@ -13,6 +13,7 @@ use crate::schmear::*;
 use crate::inverse_schmear::*;
 use crate::pseudoinverse::*;
 use crate::params::*;
+use crate::test_utils::*;
 
 #[derive(Clone)]
 pub struct FuncScatterTensor {
@@ -27,6 +28,33 @@ impl FuncScatterTensor {
             out_scatter
         };
         result
+    }
+
+    //Supposing that out (A), in (B) scatters are nxn and mxm, respectively,
+    //and given a Lx(n*m) matrix C, compute C * kron(A, B)
+    //This has better asymptotic efficiency than the naive method
+    //Delivers a result which is Lx(n*m)
+    pub fn flatten_and_multiply(&self, mat : &Array2<f32>) -> Array2<f32> {
+        let out_dim = self.out_scatter.shape()[0];
+        let in_dim = self.in_scatter.shape()[0];
+        let ret_dim = mat.shape()[0];
+
+        let in_exposed_mat = mat.clone().into_shape((ret_dim * out_dim, in_dim)).unwrap();
+        let in_transformed_mat = in_exposed_mat.dot(&self.in_scatter);
+
+        let mut in_transformed_tensor = in_transformed_mat.into_shape((ret_dim, out_dim, in_dim)).unwrap();
+        in_transformed_tensor.swap_axes(1, 2);
+        let in_transformed_tensor = in_transformed_tensor.as_standard_layout();
+
+        let out_exposed_mat = in_transformed_tensor.into_shape((ret_dim * in_dim, out_dim)).unwrap();
+        let out_transformed_mat = out_exposed_mat.dot(&self.out_scatter);
+
+        let mut out_transformed_tensor = out_transformed_mat.into_shape((ret_dim, in_dim, out_dim)).unwrap();
+        out_transformed_tensor.swap_axes(1, 2);
+        let out_transformed_tensor = out_transformed_tensor.as_standard_layout();
+
+        let result = out_transformed_tensor.into_shape((ret_dim, out_dim * in_dim)).unwrap();
+        result.into_owned()
     }
 
     pub fn flatten(&self) -> Array2<f32> {
@@ -56,3 +84,23 @@ impl FuncScatterTensor {
     }
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_flatten_and_multiply() {
+        let in_dim = 5;
+        let out_dim = 3;
+        let ret_dim = 2;
+
+        let func_scatter_tensor = random_func_scatter_tensor(out_dim, in_dim);
+        let mat = random_matrix(ret_dim, out_dim * in_dim);
+
+        let kroned = func_scatter_tensor.flatten();
+        let expected = mat.dot(&kroned);
+
+        let actual = func_scatter_tensor.flatten_and_multiply(&mat);
+
+        assert_equal_matrices(&actual, &expected);
+    }
+}
