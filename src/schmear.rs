cf4df66db903eab6949d6081ab30dfa585539519
bubble-07 <spagrab@gmail.com>
2020-12-15 18:48:21 -0500

    Switch to linear solver for computing joint probability integral mahalanobis dist

--- src/schmear.rs
+++ src/schmear.rs
@@ -6,6 +6,7 @@ use ndarray::*;
 use crate::array_utils::*;
 use ndarray_linalg::*;
 use ndarray_linalg::solveh::*;
+use ndarray_linalg::solve::*;
 use noisy_float::prelude::*;
 
 use crate::inverse_schmear::*;
@@ -21,9 +22,10 @@ const LN_TWO_PI : f32 = 1.83787706641f32;
 impl Schmear {
     pub fn joint_probability_integral(&self, other : &Schmear) -> f32 {
         let tot_covariance = &self.covariance + &other.covariance;
-        let tot_covariance_inv = pseudoinverse_h(&tot_covariance);
         let mean_diff = &other.mean - &self.mean;
-        let sq_mahalanobis_dist = mean_diff.dot(&tot_covariance_inv).dot(&mean_diff);
+
+        let mean_diff_inv = tot_covariance.solveh(&mean_diff).unwrap();
+        let sq_mahalanobis_dist = mean_diff.dot(&mean_diff_inv);
 
         let maybe_tot_covariance_sln_det = tot_covariance.sln_deth();
         let (_, tot_covariance_ln_det) = maybe_tot_covariance_sln_det.unwrap(); 
