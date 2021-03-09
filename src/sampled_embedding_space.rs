14516211d39c323c578b052aa3939dae18ff63b8
bubble-07 <spagrab@gmail.com>
2021-03-08 20:48:35 -0500

    Add best nonvec-argument-finding routine for fixed func

--- src/sampled_embedding_space.rs
+++ src/sampled_embedding_space.rs
@@ -32,6 +32,29 @@ impl SampledEmbeddingSpace {
 
         SampledTermEmbedding::FunctionEmbedding(space_info, sampled_mat)
     }
+
+    pub fn get_best_term_index_to_pass_with_value(&self, func_mat : &Array2<f32>, ret_type : TypeId,
+                                            value_field_state : &ValueFieldState)
+                                         -> (usize, f32) {
+        let mut best_arg_index = 0;
+        let mut best_arg_value = f32::NEG_INFINITY;
+        for (arg_index, arg_model) in self.models.iter() {
+            let arg_vec = &arg_model.sampled_compressed_vec; 
+            let feat_arg_vec = self.func_space_info.func_feat_info.get_features(&arg_vec);
+            let compressed_ret_vec = func_mat.dot(arg_vec);
+            let typed_ret_vec = TypedVector {
+                vec : compressed_ret_vec,
+                type_id : ret_type
+            };
+            let value = value_field_state.get_value_for_vector(&typed_ret_vec);
+            if (value > best_arg_value) {
+                best_arg_value = value;
+                best_arg_index = *arg_index;
+            }
+        }
+        (best_arg_index, best_arg_value)
+    }
+
     pub fn get_best_term_index_to_apply_with_value(&self, featurized_arg_vector : &Array1<f32>, 
                                          ret_type : TypeId, value_field_state : &ValueFieldState) 
                                          -> (usize, f32) {
